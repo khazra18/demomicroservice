d9aa54ec5afab6b9362905432a74b334b2cf9425
khazra18 <34342669+khazra18@users.noreply.github.com>
2021-08-16 19:02:11 +0530

    first commit

---
+++ src/main/java/com/demo/microservice/demomicroservice/DemomicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.microservice.demomicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemomicroserviceApplication.class, args);
+	}
+
+}
