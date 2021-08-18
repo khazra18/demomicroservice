86d919dbf441616094ecf24623da197de936901c
khazra18 <34342669+khazra18@users.noreply.github.com>
2021-08-18 19:43:27 +0530

    added exception

--- src/main/java/com/demo/microservice/demomicroservice/user/UserController.java
+++ src/main/java/com/demo/microservice/demomicroservice/user/UserController.java
@@ -9,25 +9,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.demo.microservice.demomicroservice.exception.UserNotFoundException;
+
 @RestController
 public class UserController {
 
 	@Autowired
 	private UserBeanDao userBeanDao;
-	
+
 	@GetMapping("/users")
-	public List<UserBean> retrieveAllUsers(){
+	public List<UserBean> retrieveAllUsers() {
 		return userBeanDao.findAll();
 	}
-	
+
 	@GetMapping("/users/{id}")
 	public UserBean getUserByID(@PathVariable int id) {
-		return userBeanDao.getUserByID(id);
+
+		UserBean userByID = userBeanDao.getUserByID(id);
+		if (userByID == null)
+			throw new UserNotFoundException("ID - " + id);
+
+		return userByID;
 	}
-	
+
 	@PostMapping("/addusers")
 	public UserBean addUsers(@RequestBody UserBean user) {
 		return userBeanDao.saveUser(user);
 	}
-	
+
 }
